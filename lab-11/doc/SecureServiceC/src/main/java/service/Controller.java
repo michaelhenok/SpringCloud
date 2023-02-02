bc1f051a850befe298b54d01f0bacac8d251ad48
Henok M mehreteab <michaelhenok.github.io>
2023-02-02 10:54:21 -0600

    lab-10

---
+++ lab-11/doc/SecureServiceC/src/main/java/service/Controller.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+	@GetMapping("/phone")
+	public String getPhone() {
+		return "12345678";
+	}
+
+}
+
